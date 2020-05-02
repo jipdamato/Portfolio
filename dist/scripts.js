79a111af45eac50c270530136383d376faca3a10
Jennifer Ip <jip@Jennifers-MacBook-Pro-3.local>
2020-05-02 16:04:06 -0400

    Upload dist

---
+++ dist/scripts.js
@@ -0,0 +1,31 @@
+document.querySelector('.contact-btn').addEventListener('click', () => {
+    copyEmail()
+})
+
+const copyText = document.querySelector('.hidden-input')
+const emailCopied = document.querySelector('.email-copied')
+
+function copyEmail() {
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+
+    emailCopied.classList.add('show')
+    setTimeout(() => {
+        //emailCopied.classList.remove('show')
+    }, 0 * 1000);
+
+}
+
+const menuOpt = document.querySelector('.menu-opt')
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    toggleMenu()
+})
+
+function toggleMenu() {
+    menuOpt.classList.toggle('show')
+}
